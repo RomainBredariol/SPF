56d7c41bac5957b4c3835f22b943e6e80dd68709
seb <sad_sephiroth@hotmail.com>
2019-01-14 14:29:25 -0500

    Connexion client / serveur OK

--- CodeSource/client/mainClient.c
+++ CodeSource/client/mainClient.c
@@ -6,30 +6,46 @@
 int main() {
 	char *message;		// un message echange entre le client et le serveur
 	char ip[16];		// ip de connexion au serveur
-	char port[6];		// port de connexion au serveur
+	char port[7];		// port de connexion au serveur
 	char c;			// char pour vider le stdin
 
 
-	//connexion a un serveur lecture de l'ip et du port, instancie des valeurs par defaut si rien n'est précisé
+	// message de bienvenue
 	printf("Bienvneue sur le client SPF\n\n");
+	// demande de l'ip
 	printf("Veuillez indiquer l'ip du serveur (appuyer sur entree pour localhost par defaut)\n");
 	fgets(ip, 15, stdin);
+	// si aucune ip n'est donné, utilier la valeur par defaut
 	if (strcmp(ip, "\n") == 0) { 
 		strcpy(ip,"localhost");
 	} else {
-		while ((c = getchar()) != '\n' && c != EOF) { }
+		// vider le stdin si l'utilisateur depasse
+		if (ip[15] != '\0') {
+			while ((c = getchar()) != '\n' && c != EOF) { }
+		}
+	//supprimer le new line
+	ip[strlen(ip)-1] = '\0';
 	}
+	// demander le port
 	printf("Veuillez indiquer le port (appuyer sur entree pour 1337 par defaut\n");
-	fgets(port, 5, stdin);
+	fgets(port, 7, stdin);
+	// si aucune port n'est donné, utiliser le port par défaut
 	if (strcmp(port, "\n") == 0 ) {
 		strcpy(port,"1337");
 	} else {
-		while ((c = getchar()) != '\n' && c != EOF) { }
+		// vider le stdin si l'utilisateur depasse
+		if (port[6] != '\0') {
+			while ((c = getchar()) != '\n' && c != EOF) { }
+		}
+	//supprimer le new line
+	port[strlen(port)-1] = '\0';
+
 	}
+	// message pour indiquer la connexion en cours
 	printf("\n\nConnexion à %s:%s...\n\n",ip,port);
 
 	//demande une connexion sur l'adresse localhost
-	if(Initialisation("localhost") != 1) {
+	if(InitialisationAvecService(ip,port) != 1) {
 		printf("Erreur d'initialisation\n");
 		return 1;
 	}
